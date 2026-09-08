458d811e84b9dcd72914fab946f4159f14b6f99f
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2026-09-08 06:10:02 -0400

    Update rcniquery.sql

--- test_data/rcniquery.sql
+++ test_data/rcniquery.sql
@@ -1,15 +1,17 @@
+python .\run_rcni.py --load-local-azure --issuer 70893 --year 2026 --month 01 --file-name to_70893_INDV_MONTHLYDISCREPANCY_2025_20260107070956.OUT.good.gz
+
 
 DECLARE @file_hash VARCHAR(64) =
 '791be3df3f97305ddff98879b2d9778bafebc80b8e52baf402a8c45983e19924';
-
-DECLARE @failed_run UNIQUEIDENTIFIER =
-'c0e9161f-9de9-40d1-a3b4-642d6630dbca';
 
 SELECT COUNT(*) AS Raw_Rows
 FROM dbo.rcni_raw
 WHERE file_hash = @file_hash;
 
-SELECT COUNT(*) AS Stage_Rows
+SELECT
+    load_run_id,
+    COUNT(*) AS Stage_Rows
 FROM dbo.rcni_stage
-WHERE load_run_id = @failed_run
-  AND file_hash = @file_hash;
+WHERE file_hash = @file_hash
+GROUP BY load_run_id
+ORDER BY load_run_id;
