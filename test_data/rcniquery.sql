dc6ef1abf9f5ee25b9d3de4744ec87968ce88e38
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2026-09-08 06:02:40 -0400

    Refactor SQL query to simplify structure
    Removed unnecessary SELECT statements and adjusted FROM clauses.

--- test_data/rcniquery.sql
+++ test_data/rcniquery.sql
@@ -1,12 +1,3 @@
-SELECT
-    DB_NAME() AS CurrentDatabase,
-    @@SERVERNAME AS CurrentServer;
-
-SELECT
-    OBJECT_ID('dbo.rcni_raw') AS RawTableObjectID,
-    OBJECT_ID('dbo.rcni_stage') AS StageTableObjectID;
-
-
 
 DECLARE @file_hash VARCHAR(64) =
 '791be3df3f97305ddff98879b2d9778bafebc80b8e52baf402a8c45983e19924';
@@ -15,10 +6,10 @@ DECLARE @failed_run UNIQUEIDENTIFIER =
 'c0e9161f-9de9-40d1-a3b4-642d6630dbca';
 
 SELECT COUNT(*) AS Raw_Rows
-FROM [dbo].[rcni_raw]
-WHERE [file_hash] = @file_hash;
+FROM dbo.rcni_raw
+WHERE file_hash = @file_hash;
 
 SELECT COUNT(*) AS Stage_Rows
-FROM [dbo].[rcni_stage]
-WHERE [load_run_id] = @failed_run
-  AND [file_hash] = @file_hash;
+FROM dbo.rcni_stage
+WHERE load_run_id = @failed_run
+  AND file_hash = @file_hash;
