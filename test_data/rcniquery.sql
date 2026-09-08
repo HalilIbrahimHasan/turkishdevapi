7b2a2511caa386a58e831fa0c88a1fd2d98c496f
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2026-09-08 05:52:34 -0400

    Add SQL script for counting rows by file hash

---
+++ test_data/rcniquery.sql
@@ -0,0 +1,14 @@
+DECLARE @file_hash CHAR(64) =
+'791be3df3f97305ddff98879b2d9778bafebc80b8e52baf402a8c45983e19924';
+
+DECLARE @failed_run UNIQUEIDENTIFIER =
+'c0e9161f-9de9-40d1-a3b4-642d6630dbca';
+
+SELECT COUNT(*) AS Raw_Rows
+FROM dbo.rcni_raw
+WHERE file_hash = @file_hash;
+
+SELECT COUNT(*) AS Stage_Rows
+FROM dbo.rcni_stage
+WHERE load_run_id = @failed_run
+  AND file_hash = @file_hash;
